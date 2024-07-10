f98d4b0ad405f816fb6807110d67ed383418d82b
dunky-star <dunkygeoffrey39@gmail.com>
2024-07-10 13:56:49 -0500

    Go and NoSQL - MongoDB

---
+++ services/user.service.go
@@ -0,0 +1,12 @@
+package services
+
+import "dunky-star/gin-mongodb-apis/models"
+
+//  Services contract for CRUD operations on User struct
+type UserService interface {
+	CreateUser(*models.User) error
+	GetUser(*string) (*models.User, error)
+    GetAll([] *models.User) error
+    UpdateUser(*models.User) error
+	DeleteUser(*string) error
+}

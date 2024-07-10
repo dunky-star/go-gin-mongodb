ab7149fc0e8c5981f06cd24a4fba2cfa7f38e1c1
dunky-star <dunkygeoffrey39@gmail.com>
2024-07-10 14:02:00 -0500

    Go and NoSQL - MongoDB

--- services/user.service.go
+++ services/user.service.go
@@ -6,7 +6,7 @@ import "dunky-star/gin-mongodb-apis/models"
 type UserService interface {
 	CreateUser(*models.User) error
 	GetUser(*string) (*models.User, error)
-    GetAll([] *models.User) error
+    GetAll() ([] *models.User, error)
     UpdateUser(*models.User) error
 	DeleteUser(*string) error
 }
